bb976ff24c6dee386074e070d71725d77219052d
Nailson97 <nailsonsousa97@gmail.com>
2021-10-30 18:43:07 -0300

    atualisando commit

--- database/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-
-
-const connection = new Sequelize('guia perguntas','root','1234567',{
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
-module.exports = connection
